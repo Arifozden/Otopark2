feef61d4c581ddb5bf2e819d86433068c49f8a6e
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-05-19 21:53:37 +0200

    updated

---
+++ src/main/java/otopark/otopark2/Otopark2Application.java
@@ -0,0 +1,13 @@
+package otopark.otopark2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Otopark2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Otopark2Application.class, args);
+    }
+
+}
---
+++ src/test/java/otopark/otopark2/Otopark2ApplicationTests.java
@@ -0,0 +1,13 @@
+package otopark.otopark2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Otopark2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
